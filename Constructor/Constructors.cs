8df562a54e9d6ea164ad37b9dd139f5acfe3a014
Uzairrr1 <mohd.uzair@knoldus.com>
2023-02-02 17:45:02 +0530

    Adding two files

---
+++ Constructor/Constructors.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+
+class Student 
+{
+  int RollNo;
+  string Name;
+  int Age;
+
+  public Student()
+  {
+    Console.WriteLine("Constructor is invoked");
+  }
+  public Student(int RollNo, string Name, int Age)
+  {
+    this.RollNo = RollNo;
+    this.Name = Name;
+    this.Age = Age;
+  }
+  public void GetStudent()
+  {
+  Console.WriteLine("Your roll no is: {0}", this.RollNo);
+  Console.WriteLine("Your name is: {0}", this.Name);
+  Console.WriteLine("Your age is: {0}", this.Age);
+  }
+  static void Main(String[] args)
+  {
+    // Student karan = new Student();
+    Student Uzair = new Student(22,"Uzair",23);
+    Uzair.GetStudent();
+  }
+}
